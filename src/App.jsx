66865c8b0764086919b6a9bade5cbfb7179e3e2a
Matt Baxter <mbx33@hotmail.com>
2023-05-19 07:25:47 +0300

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import styled from '@emotion/styled';
+
+function App() {
+	return (
+		<Main>
+			<h1>Multi Step Form</h1>
+		</Main>
+	);
+}
+
+export default App;
+
+const Main = styled.main`
+	background-color: var(--neutral-cool);
+	color: #fff;
+	padding: 1rem;
+	text-align: center;
+	height: 100vh;
+`;
